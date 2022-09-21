73512df557c5d2250be32532f99069f33391407e
Clauribi <claudiarivasbielsa@gmail.com>
2022-09-21 11:32:52 +0200

    validaciones

--- src/main/java/com/example/demo/controller/VendedorController.java
+++ src/main/java/com/example/demo/controller/VendedorController.java
@@ -1,9 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class VendedorController {
+
+    @PostMapping("/vendedores")
+    public void altaVendedores(@Valid @RequestBody VendedorInput vendedor) {
+
+    }
 
 }
--- src/main/java/com/example/demo/controller/VendedorUpdate.java
+++ src/main/java/com/example/demo/controller/VendedorUpdate.java
@@ -1,4 +1,50 @@
 package com.example.demo.controller;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
 public class VendedorUpdate {
+    @NotNull(message = "Nombre is null")
+    @NotBlank(message = "Nombre is empty")
+    private String nombre;
+    @NotNull(message = "Direccion is null")
+    @NotBlank(message = "Direccion is empty")
+    private String direccion;
+    @Positive(message = "telefono is negative")
+    private int telefono;
+
+    public VendedorUpdate(String nombre, String direccion, int telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+    public VendedorUpdate(){
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }

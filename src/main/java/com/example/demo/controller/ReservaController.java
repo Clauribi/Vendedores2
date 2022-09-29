66de4bc22d42fadf004dc8754f37e2b2e3e2cb77
Clauribi <claudiarivasbielsa@gmail.com>
2022-09-29 10:41:18 +0200

    probando

--- src/main/java/com/example/demo/controller/ReservaController.java
+++ src/main/java/com/example/demo/controller/ReservaController.java
@@ -11,18 +11,18 @@ public class ReservaController {
     private Concesionario concesionario = new Concesionario();
 
 
-    @PostMapping("/reservas/coche/{matricula}/cliente/{dni}")
+ /*   @PostMapping("/reservas/coche/{matricula}/cliente/{dni}")
     public String hacerReserva(@PathVariable String matricula, @PathVariable String dni) {
         try {
             ArrayList<Reserva> listaReservas = new ArrayList<>();
             for (Reserva reserva : concesionario.getListadoReservas().values()) {
                 listaReservas.add(reserva);
             } return listaReservas.toString();
-            
+
         } catch (Exception e) {
             e.getMessage();
         } return null;
-    }
+    }*/
 
     @DeleteMapping("/reservas/coche/{matricula/cliente/{dni}")
     public void cancelarReserva(@PathVariable String matricula, @PathVariable String dni) {
